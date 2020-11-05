195ae712efa57dce2623e74d722b3bf7325ef368
Smak80 <smaklets@kpfu.ru>
2020-11-05 13:26:13 +0300

    Перцептрон. С вычислением ошибок на тренировочной и тестовой выборках + отображение графиков

--- MLPerceptron.py
+++ MLPerceptron.py
@@ -1,22 +1,28 @@
 import numpy as np
 
+import data_loader as DataLoader
+
+
 class MLP:
     __eta = 0.005
     __a = 1
     __b = 1
 
     def __init__(self,
-                 inp: list,
-                 out: list,
+                 ld: DataLoader.loader,
                  neuronNum: tuple = (4, 3)
                  ):
         # Количество слоев
         self.__layers = 2 + len(neuronNum)
         # Количество нейронов на каждом слое
+        inp = ld.getTrainInp()
+        out = ld.getTrainOut()
         nN = [len(inp[0]), len(out[0])]
         self.__nN = np.insert(nN, 1, neuronNum)
         self.__inp = np.array(inp)
         self.__out = np.array(out)
+        self.__tst_inp = np.array(ld.getTestInp())
+        self.__tst_out = np.array(ld.getTestOut())
         self.__w = [np.random.rand(
             self.__nN[i] + 1,
             self.__nN[i + 1] +
@@ -41,10 +47,11 @@ class MLP:
     def linActDer(self, x):
         return np.array(1)
 
-
     def learn(self,
               epoches=1000,
               epsilon=0.002):
+        e_full_tr = []
+        e_full_ts = []
         # Индуцированное локальное поле
         v = np.array([None for i in range(self.__layers)])
         # Слои (выходы из слоёв)
@@ -59,17 +66,21 @@ class MLP:
         out = self.__out
         # Счетчик эпох
         k = 0
-        #Полная ошибка сети
-        err_n = epsilon+1
-        #Начало процесса обучения
+        # Полная ошибка сети на тестовой выборке
+        err_n = epsilon + 1
+        # Полная ошибка сети на обучающей выборке
+        tr_err_n = epsilon + 1
+        # Начало процесса обучения
         while k < epoches and err_n > epsilon:
             err_n = 0
+            tr_err_n = 0
             k += 1
-            #Проход по обучающей выборке
+            # Проход по обучающей выборке
             for i in range(len(inp)):
                 l[0] = np.array([np.insert(inp[i], 0, 1)])  # Попробовать вынести за цикл while
                 # Прямой проход по сети
                 for j in range(1, self.__layers - 1):
+                    # индуцированное локальное поле
                     v[j] = np.dot(l[j - 1], self.__w[j - 1])
                     # проход по скрытым слоям
                     l[j] = self.nonLinAct(v[j])
@@ -82,33 +93,39 @@ class MLP:
                 # Обратный проход по сети
                 l_err[self.__layers - 2] = out[i] - l[self.__layers - 1]
                 # Накопление общей ошибки сети на данной эпохе
-                err_n += 0.5 * (out[i] - l[self.__layers - 1]) ** 2
+                tr_err_n += 0.5 * (out[i] - l[self.__layers - 1]) ** 2
                 # Нахождение \delta_k
                 l_delta[self.__layers - 2] = \
                     np.array([l_err[self.__layers - 2][0] * (
                         self.linActDer(v[self.__layers - 1])
                     )])
-                #Нахождение \delta_j
-                for j in range(self.__layers-2, 0, -1):
+                # Нахождение \delta_j
+                for j in range(self.__layers - 2, 0, -1):
                     l_err[j - 1] = np.dot(l_delta[j], self.__w[j].T)
                     l_delta[j - 1] = l_err[j - 1] * self.nonLinActDer(v[j])
-                #Определение изменения весовых коэффициентов \Delta w
+                # Определение изменения весовых коэффициентов \Delta w
                 deltaW = [self.__eta * np.dot(l_delta[j].T, l[j])
-                          for j in range(self.__layers-1)]
-                for j in range(0, self.__layers -1):
+                          for j in range(self.__layers - 1)]
+                for j in range(0, self.__layers - 1):
                     self.__w[j] += deltaW[j].T
-            #Вычисление общей ошибки сети для данной эпохи
-            err_n /= len(inp)
-            print("Epoche", k, "Error=", err_n)
-
+            # Вычисление общей ошибки сети для данной эпохи
+            tr_err_n /= len(inp)
+            outt = self.calc(self.__tst_inp)
+            ln = len(outt)
+            soutt = np.array([self.__tst_out[i][0] for i in range(len(self.__tst_out))])
+            err_n = np.sum(0.5 * (soutt - outt) ** 2) / ln
+            e_full_tr.append(tr_err_n)
+            e_full_ts.append(err_n)
+            print("Epoche", k, "Error (train)=", tr_err_n, "Error test=", err_n)
+        return e_full_tr, e_full_ts
 
     #Вычисление выходов по входом обученной сетью
-    def calc(self, inps: list):
+    def calc(self, inps):
         outs = np.array([])
-        #Для каждого входного значения
+        # Для каждого входного значения
         for i in range(len(inps)):
             inp = np.array([np.insert(inps[i], 0, 1)])
-            #Прямой проход по сети (все слои, кроме последнего)
+            # Прямой проход по сети (все слои, кроме последнего)
             for lr in range(self.__layers - 2):
                 inp = self.nonLinAct(np.dot(inp, self.__w[lr]))
             # Получение результата на последнем слое
--- main.py
+++ main.py
@@ -5,19 +5,28 @@ from MLPerceptron import MLP
 
 
 def plot2d():
-    ld = dl.loader()
+    ld = dl.loader(trainPercent=75)
     tri = ld.getTrainInp()
     tro = ld.getTrainOut()
     tsi = ld.getTestInp()
     tso = ld.getTestOut()
-    mlp = MLP(tri, tro, (5, 10))
-    mlp.learn(2000, epsilon=0.002)
-    plt.plot(tri, tro, "r+")
+    mlp = MLP(ld, (12, 30))
+    e_tr, e_ts = mlp.learn(2500, epsilon=0.002)
+    e_tr = [e_tr[i][0][0] for i in range(len(e_tr))]
+    e_tr_x = [i for i in range(1, len(e_tr) + 1)]
+    e_ts_x = [i for i in range(1, len(e_ts) + 1)]
+    f = plt.figure()
+    fa1 = f.add_subplot(3, 1, 1)
+    fa1.plot(tri, tro, "r+")
     out = mlp.calc(tri)
-    plt.plot(tri, out, "b-")
+    fa1.plot(tri, out, "bo")
     out = mlp.calc(tsi)
-    plt.plot(tsi, tso, "yo")
-    plt.plot(tsi, out, "go")
+    fa1.plot(tsi, tso, "yo")
+    fa1.plot(tsi, out, "go")
+    fa2 = f.add_subplot(3, 1, 2)
+    fa2.plot(e_tr_x, e_tr, "r-")
+    fa3 = f.add_subplot(3, 1, 3)
+    fa3.plot(e_ts_x, e_ts, "b-")
     plt.show()
 
 
